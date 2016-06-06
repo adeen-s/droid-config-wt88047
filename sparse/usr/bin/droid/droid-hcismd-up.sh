4159e3edb5ede0fefe98652e59b2ea8622298d24
adeen-s <adeens357@gmail.com>
2016-06-06 12:26:43 +0530

    Change hcismd-up script to get valid bt mac

--- sparse/usr/bin/droid/droid-hcismd-up.sh
+++ sparse/usr/bin/droid/droid-hcismd-up.sh
@@ -5,10 +5,24 @@
 # not work, so we loop.
 MAXTRIES=15
 
-seq 1 $MAXTRIES | while read i ; do
+#setprop bluetooth.hciattach true
+setprop ro.qualcomm.bt.hci_transport smd
+setprop qcom.bt.dev_power_class 2
+setprop qcom.bt.le_dev_pwr_class 2
+
+i=1
+while [ ! $i -gt $MAXTRIES ]  ; do
     echo 1 > /sys/module/hci_smd/parameters/hcismd_set
     if [ -e /sys/class/bluetooth/hci0 ] ; then
         # found hci0, exit successfully
+        echo 0 > /sys/module/hci_smd/parameters/hcismd_set
+        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
+        echo "BT MAC: $bt_mac"
+        if [ ! -z "$bt_mac" ] ; then
+            echo $bt_mac > /var/lib/bluetooth/board-address
+            echo "BT MAC: $bt_mac"
+        fi
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
         exit 0
     fi
     sleep 1
